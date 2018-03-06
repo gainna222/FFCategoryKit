a7208e6e98ef0d3d981420bd05d1844b2b61457c
edward.qiu@aolc.cn <edward.qiu@aolc.cn>
2018-03-06 10:10:06 +0800

    Initial commit

---
+++ Example/FFCategoryKit/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFCategoryKit
+//
+//  Created by edward.qiu@aolc.cn on 03/06/2018.
+//  Copyright (c) 2018 edward.qiu@aolc.cn. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FFCategoryKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFCategoryKit
+//
+//  Created by edward.qiu@aolc.cn on 03/06/2018.
+//  Copyright (c) 2018 edward.qiu@aolc.cn. All rights reserved.
+//
+
+@import UIKit;
+#import "FFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FFAppDelegate class]));
+    }
+}
